f74a5f1275f1f89a9a016c11c0be3eab03d4c890
buidanhtaun <buidanhtuan221299@gmail.com>
2020-02-24 20:39:06 +0700

    MainActivity

--- app/src/main/java/com/buidanh/test1/MainActivity.kt
+++ app/src/main/java/com/buidanh/test1/MainActivity.kt
@@ -2,11 +2,20 @@ package com.buidanh.test1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        button.setOnClickListener {
+            var number1:Int = input1.text.toString().toInt()
+            var number2:Int = input2.text.toString().toInt()
+
+            var result:Int = number1 + number2
+            output.text = result.toString()
+        }
     }
 }
