77c4613f1ab5f79b27745fbee95fb3b8ce9a4519
loannth <loannth@sapo.vn>
2020-02-26 09:46:36 +0700

    review code loan

--- test3/app/src/main/java/com/buidanh/myapplication/UserAdapter.kt
+++ test3/app/src/main/java/com/buidanh/myapplication/UserAdapter.kt
@@ -15,9 +15,15 @@ import androidx.recyclerview.widget.RecyclerView
  *
  * @author Guilherme Biff Zarelli
  */
+
+/*
+Chị đang thấy em quá làm dụng lambda trong này
+Có những phần không cần dùng em lại dùng
+
+ */
 class UserAdapter(
     //I decided to create a functional interface instead of a new class.
-    val onClickItem: (view: View, user: User) -> Unit
+    val onClickItem: (view: View, user: User) -> Unit// chỉ nên truyền nhưng param thực sự cần dùng
 ) : ListAdapter<User, UserViewHolder>(UserDiffCallback()) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
@@ -41,6 +47,9 @@ class UserAdapter(
 /**
  * Callback for calculating the diff between two non-null items in a list.
  */
+/**
+ * em có hiểu nó nghĩa là gì không
+ */
 class UserDiffCallback : DiffUtil.ItemCallback<User>() {
     override fun areItemsTheSame(oldItem: User, newItem: User): Boolean = oldItem.userId == newItem.userId
     override fun areContentsTheSame(oldItem: User, newItem: User): Boolean = oldItem.name == newItem.name
@@ -53,6 +62,8 @@ class UserViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     private var textViewName: TextView = view.findViewById(R.id.name)
 
     fun bind(user: User, onClickListener: View.OnClickListener) {
+        // dùng adapterposition nha k dùng position từ onBindViewHolder sau này
+        // làm các bài về thay đổi trạng thái nhiều item có thể hay bị sai đó
         textViewName.text = user.name
         itemView.setOnClickListener(onClickListener)
     }
